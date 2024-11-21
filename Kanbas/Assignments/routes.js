34b06ae28ffd88595e3e2c4625906cda7c8b1519
Vicky Daniel <vicky16898@gmail.com>
2024-11-21 00:06:47 -0500

    Setup kanbas node.js server

---
+++ Kanbas/Assignments/routes.js
@@ -0,0 +1,15 @@
+import * as assignmentDao from "./dao.js";
+export default function AssignmentRoutes(app) {
+    app.delete("/api/assignments/:assignmentId", (req, res) => {
+        const { assignmentId } = req.params;
+        assignmentDao.deleteAssignment(assignmentId);
+        res.sendStatus(204);
+    });
+    app.put("/api/assignments/:assignmentId", (req, res) => {
+        const { assignmentId } = req.params;
+        const assignmentUpdates = req.body;
+        assignmentDao.updateModule(assignmentId, assignmentUpdates);
+        res.sendStatus(204);
+    });
+
+}
---
+++ Lab5/WorkingWithObjects.js
@@ -0,0 +1,30 @@
+const assignment = {
+    id: 1, title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10", completed: false, score: 0
+};
+export default function WorkingWithObjects(app) {
+    app.get("/lab5/assignment", (req, res) => {
+        res.json(assignment);
+    });
+    app.get("/lab5/assignment/title", (req, res) => {
+        res.json(assignment.title);
+    });
+    app.get("/lab5/assignment/title/:newTitle", (req, res) => {
+        const { newTitle } = req.params;
+        assignment.title = newTitle;
+        res.json(assignment);
+    });
+    app.get("/lab5/assignment/score/:score", (req, res) => {
+        const { score } = req.params;
+        assignment.score = score;
+        res.json(assignment);
+    });
+    app.get("/lab5/assignment/completed/:status", (req, res) => {
+        const { status } = req.params;
+        assignment.completed = status;
+        res.json(assignment);
+    });
+
+
+};
